35463f8458741c99b1de225e9bb63d720a793544
Kalimuthu P <1995kalimuthu@gmail.com>
2020-11-23 22:51:46 +0530

    children for the grid is added

--- dash_grid/DashGrid.py
+++ dash_grid/DashGrid.py
@@ -5,23 +5,20 @@ from dash.development.base_component import Component, _explicitize_args
 
 class DashGrid(Component):
     """A DashGrid component.
-GridComponent is an example component.
-It takes a property, `label`, and
+GridComponent is an dash component.
+It takes a property, `id`, and children to
 displays it.
-It renders an input with the property `value`
-which is editable by the user.
 
 Keyword arguments:
-- id (string; optional): The ID used to identify this component in Dash callbacks.
-- label (string; required): A label that will be printed when this component is rendered.
-- value (string; optional): The value displayed in the input."""
+- children (a list of or a singular dash component, string or number; optional): The children components displayed inside the grid.
+- id (string; optional): The ID used to identify this component in Dash callbacks."""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, label=Component.REQUIRED, value=Component.UNDEFINED, className=Component.UNDEFINED, items=Component.UNDEFINED, cols=Component.UNDEFINED, rowHeight=Component.UNDEFINED, onLayoutChange=Component.UNDEFINED, verticalCompact=Component.UNDEFINED, preventCollision=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'label', 'value']
+    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, items=Component.UNDEFINED, cols=Component.UNDEFINED, rowHeight=Component.UNDEFINED, onLayoutChange=Component.UNDEFINED, verticalCompact=Component.UNDEFINED, preventCollision=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id']
         self._type = 'DashGrid'
         self._namespace = 'dash_grid'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'label', 'value']
+        self.available_properties = ['children', 'id']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -29,8 +26,8 @@ Keyword arguments:
         _locals.update(kwargs)  # For wildcard attrs
         args = {k: _locals[k] for k in _explicit_args if k != 'children'}
 
-        for k in ['label']:
+        for k in []:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
-        super(DashGrid, self).__init__(**args)
+        super(DashGrid, self).__init__(children=children, **args)
--- usage.py
+++ usage.py
@@ -2,22 +2,59 @@ import dash_grid
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
-
+import dash_core_components as dcc
+import dash_daq as daq
+import pandas as pd
 app = dash.Dash(__name__)
 
+import plotly.express as px
+
+df = px.data.iris() # iris is a pandas DataFrame
+fig = px.scatter(df, x="sepal_width", y="sepal_length")
+import plotly.graph_objs as go
+fig1 = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
+df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
+df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
+fig2 = px.pie(df, values='pop', names='country', title='Population of European continent')
+fig3 = px.imshow([[1, 20, 30],
+                 [20, 1, 60],
+                 [30, 60, 1]])
 app.layout = html.Div([
     dash_grid.DashGrid(
-        id='input',
-        value='my-value',
-        label='my-label'
+        id='syncdashboard',
+        children=[
+             html.P(id = "status1",children=["init-status"] ),
+             html.P(id = "status2",children=["init-power"] ),
+             daq.LEDDisplay(
+               id='my-LED-display',
+               label="Default",
+               value=6
+             ),
+             daq.LEDDisplay(
+               id='my-LED-display1',
+               label="Default",
+               value=16
+             ),
+              dcc.Graph(
+                id='example-graph',
+                figure=fig
+              ),
+               dcc.Graph(
+                id='example-graph1',
+                figure=fig1
+              ),
+              dcc.Graph(
+                id='example-graph2',
+                figure=fig2
+              ),
+              dcc.Graph(
+                id='example-graph3',
+                figure=fig3
+              )
+]
     ),
     html.Div(id='output')
 ])
-
-
-@app.callback(Output('output', 'children'), [Input('input', 'value')])
-def display_output(value):
-    return 'You have entered {}'.format(value)
 
 
 if __name__ == '__main__':
