91ba6ee8f712b10ea0764e2bed5f6ce75a3e89b6
Kalimuthu P <1995kalimuthu@gmail.com>
2020-11-27 14:42:17 +0530

    editable option added

--- dash_grid/DashGrid.py
+++ dash_grid/DashGrid.py
@@ -12,14 +12,15 @@ displays it.
 Keyword arguments:
 - children (a list of or a singular dash component, string or number; optional): The children components displayed inside the grid.
 - id (string; optional): The ID used to identify this component in Dash callbacks.
+- editable (boolean; optional)
 - position (list; optional): The layout of the  components displayed inside the grid."""
     @_explicitize_args
-    def __init__(self, children=None, id=Component.UNDEFINED, position=Component.UNDEFINED, className=Component.UNDEFINED, items=Component.UNDEFINED, cols=Component.UNDEFINED, rowHeight=Component.UNDEFINED, verticalCompact=Component.UNDEFINED, preventCollision=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['children', 'id', 'position']
+    def __init__(self, children=None, id=Component.UNDEFINED, editable=Component.UNDEFINED, position=Component.UNDEFINED, className=Component.UNDEFINED, items=Component.UNDEFINED, cols=Component.UNDEFINED, rowHeight=Component.UNDEFINED, verticalCompact=Component.UNDEFINED, isDraggable=Component.UNDEFINED, isResizable=Component.UNDEFINED, preventCollision=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'editable', 'position']
         self._type = 'DashGrid'
         self._namespace = 'dash_grid'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['children', 'id', 'position']
+        self.available_properties = ['children', 'id', 'editable', 'position']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
